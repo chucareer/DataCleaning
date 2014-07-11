a4d189e3ec8af11220dff5f7788a2cf7c143022e
chucareer <chucareer@users.noreply.github.com>
2014-07-11 12:06:26 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-run_analysis.R
-
 ## Create one R script called run_analysis.R that does the following:
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
